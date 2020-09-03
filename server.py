f7f283e4190207088c9e1d4fedd64a2759d6d1e5
Massu <imomushi.pipi@gmail.com>
2020-09-03 23:23:31 +0900

    [add] add comment

--- server.py
+++ server.py
@@ -7,32 +7,39 @@ from ledctrl import LEDCtrl
 
 # ファイルアップロード先のディレクトリ
 UPLOAD_FOLDER = './uploads'
+
 # uploadを許可する拡張子
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
 
+# Flaskのインスタンス生成
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# LEDのインスタンス生成
+# LEDインスタンス格納用
 led = None
 
 def show_uploadimage(led, imgpath):
+    """アップロードした画像をLEDに表示
+    """
     led.clear()
     img = led.loadimg(imgpath)
     led.showimage(img)
 
-# 拡張子のチェック
 def check_file(filename):
+    """拡張子のチェック
+    """
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global led
+    # 初回のみ，LEDのインスタンス生成
     if led == None:
         led = LEDCtrl()
         led.setup()
 
+    # LED表示をクリア
     led.clear()
 
     if request.method == 'POST':
@@ -52,16 +59,17 @@ def index():
             filename = secure_filename(img_file.filename)
             fileformat = filename.split(".")[1]
 
+            # ページに表示する画像のフォーマットを作成
             img_base64 = base64.b64encode(img_file.read())
             img_base64_str = "data:image/{};base64,".format(fileformat) + str(img_base64).split("'")[1]
 
-            #img_bytes.seek(0)
+            # LED表示用の画像のフォーマットを作成
             data = str(img_base64).split("'")[1]
 
             # LEDに画像表示
             show_uploadimage(led, io.BytesIO(base64.b64decode(data)))
 
-            
+            # ページに画像表示
             return render_template('index.html', img_base=img_base64_str)
 
         else:
